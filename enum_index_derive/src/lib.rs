58560115bc0c5dfa211f9990be906c17c0c6cbc1
Alexander Walter <alexander@wackazong.com>
2024-11-20 22:28:33 +0100

    pulled in pull requests

--- enum_index_derive/src/lib.rs
+++ enum_index_derive/src/lib.rs
@@ -1,17 +1,16 @@
 extern crate syn;
 #[macro_use]
 extern crate quote;
-extern crate proc_macro;
 extern crate proc_macro2;
-use quote::format_ident;
-use syn::{parse_macro_input, DeriveInput};
+extern crate proc_macro;
+use proc_macro::TokenStream;
 
 #[proc_macro_derive(EnumIndex)]
-pub fn enum_index(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
+pub fn enum_index(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
 
     let tokens = impl_enum_index(&ast);
-    proc_macro::TokenStream::from(tokens)
+    tokens.into()
 }
 
 
@@ -20,7 +19,7 @@ fn impl_enum_index(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
 
     let variants = match ast.data {
-        syn::Data::Enum(ref v) => &v.variants,
+        syn::Data::Enum(ref data) => &data.variants,
         _ => panic!("EnumIndex can be only implemented for Enums")
     };
 
@@ -30,10 +29,10 @@ fn impl_enum_index(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
         use syn::Fields::*;
         let ident = &variant.ident;
 
-        let params = match variant.fields {
-            Unit => format_ident!(""),
-            Unnamed(..) => format_ident!("(..)"),
-            Named(..) => format_ident!("{{..}}")
+        let params = match &variant.fields {
+            Unit => proc_macro2::TokenStream::new(),
+            Unnamed(..) => syn::parse_str("(..)").unwrap(),
+            Named(..) => syn::parse_str("{..}").unwrap(),
         };
 
         let index = matches.len();
@@ -53,11 +52,11 @@ fn impl_enum_index(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
 
 
 #[proc_macro_derive(IndexEnum)]
-pub fn index_enum(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
+pub fn index_enum(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
 
     let tokens = impl_index_enum(&ast);
-    proc_macro::TokenStream::from(tokens)
+    tokens.into()
 }
 
 
@@ -66,24 +65,15 @@ fn impl_index_enum(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
 
     let variants = match ast.data {
-        syn::Data::Enum(ref v) => &v.variants,
+        syn::Data::Enum(ref data) => &data.variants,
         _ => panic!("IndexEnum can be only implemented for Enums")
     };
 
     let mut index_matches = Vec::new();
-    let mut index : usize = 0;
 
-    for variant in variants {
+    for (index, variant) in variants.into_iter().enumerate() {
         use syn::Fields::*;
         let ident = &variant.ident;
-        match variant.discriminant {
-            None => {},
-            Some((_, syn::Expr::Lit(syn::ExprLit { lit: syn::Lit::Int(ref i), .. }))) => {
-                index = i.base10_parse::<usize>().unwrap();
-            },
-            Some(_) => panic!("Unhandled enum discriminant!"),
-        };
-
         match variant.fields {
             Unit => {
                 index_matches.push(quote! { #index => Some(#name::#ident) });
@@ -108,7 +98,6 @@ fn impl_index_enum(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
                 index_matches.push(quote! { #index => Some(#name::#ident{#(#initialized_fields),*})});
             }
         }
-        index += 1;
     }
 
     quote!{
